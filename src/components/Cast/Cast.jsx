ccc4265ea25b15c7105767a6185f05bf54d6103e
kkot04 <144848298+kkot04@users.noreply.github.com>
2023-12-26 16:07:42 +0200

    fixed

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,36 +1,39 @@
-import React, {useState, useEffect}from 'react'
-import { useParams } from 'react-router-dom'
-import { fetchMoviesCast } from 'API/MovieApi'
-import s from './Cast.module.css'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMoviesCast } from 'API/MovieApi';
 
 
-export const Cast = () => {
-  const {movieId} = useParams()
-  const[cast, setCast] = useState([])
+const Cast = () => {
+  const { movieId } = useParams();
+  const [cast, setCast] = useState([]);
 
   useEffect(() => {
-    fetchMoviesCast(movieId).then(res => setCast(res.cast))
-  }, [movieId])
-
-  return(
+    fetchMoviesCast(movieId).then(res => setCast(res.cast));
+  }, [movieId]);
+  return (
     <>
-    <ul className={s.list}>
+      <ul>
         {cast.length !== 0 ? (
-            cast.map( item => (
-                <li key={item.id} className={s.item}>
-                    
-                    <h3 className={s.title}>{item.name}</h3>
-                    <span className={s.text}>
-                        <b>Role:</b> {item.character}
-                    </span>
-                </li>
-             ) )
-        ): (<h2> We do not have information about the cast, or they have wished to
-            remain anonymous.</h2>)}
-        
-    </ul>
+          cast.map(item => (
+            <li key={item.id}>
+              <div>
+                <h3>{item.name}</h3>
+                <span>
+                  <b>Role:</b> {item.character}
+                </span>
+              </div>
+            </li>
+          ))
+        ) : (
+          <h2>
+            We do not have information about the cast, or they have wished to
+            remain anonymous.
+          </h2>
+        )}
+      </ul>
     </>
-  )
+  );
+};
+export default Cast;
 
 
-}
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,30 +1,34 @@
-import React, {useState, useEffect} from 'react'
-import { useParams } from 'react-router-dom'
-import { fetchMoviesReviews } from 'API/MovieApi'
-import s from './Reviews.module.css'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMoviesReviews } from 'API/MovieApi';
 
 export const Reviews = () => {
-    const {movieId} = useParams()
-    const [review, setReview] = useState([]) 
+  const { movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
 
-    useEffect(() => {
-        fetchMoviesReviews(movieId).then(res => {setReview(res)})
-    }, [movieId])
+  useEffect(() => {
+    fetchMoviesReviews(movieId).then(res => setReviews(res));
+  }, [movieId]);
+  return (
+    <div>
+      <ul>
+        {reviews.length !== 0 ? (
+          reviews.map(item => (
+            <li key={item.id}>
+              <span>{item.author}</span>
+              <p>{item.content}</p>
+            </li>
+          ))
+        ) : (
+          <h2>
+            There are currently no reviews. This movie has not yet had time to
+            talk about itself, but we hope that the reviews will be positive.
+          </h2>
+        )}
+      </ul>
+    </div>
+  );
+};
 
+export default Reviews;
 
-    return(
-        <div>
-            <ul className={s.list}>
-                {review.length !== 0 ? (
-                review.map(item => (
-                    <li key={item.id} className={s.item}> 
-                    <span className={s.span}>{item.author}</span>
-                    <p className={s.text}>{item.content}</p>
-                    </li>
-                ))
-            ) : (<h2 className={s.title}>There are currently no reviews. This movie has not yet had time to
-                talk about itself, but we hope that the reviews will be positive.</h2>)}
-            </ul>
-        </div>
-    )
-}
